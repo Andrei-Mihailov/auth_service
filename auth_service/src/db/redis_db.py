9f0f34a0d5977d2a5d1993c21d1e0c69edd1dbb1
Andrei Mihailov <andrew19220@yandex.ru>
2024-04-14 02:25:19 +0300

    добавил функции чтения/записи данных токенов, пользователей, массивов

--- auth_service/src/db/redis_db.py
+++ auth_service/src/db/redis_db.py
@@ -1,3 +1,5 @@
+import json
+
 from typing import Union
 
 import backoff
@@ -22,8 +24,41 @@ class RedisCache(Cache):
         return data
 
     @backoff.on_exception(backoff.expo, conn_err_redis, max_tries=5)
-    async def set(self, key, value, expire):
-        await self.redis.set(key, value, expire)
+    async def set(self, key, value, expire=None):
+        await self.redis.set(key, value)
+        if expire:
+            await self.redis.expire(key, expire)
+
+    async def set_jwt(self, token_type, user_id, token, expire=None):
+        key = f"jwt_tokens:{token_type}:{user_id}"
+        await self.set(key, token, expire)
+
+    async def get_jwt(self, token_type, user_id):
+        key = f"jwt_tokens:{token_type}:{user_id}"
+        await self.get(key)
+
+    @backoff.on_exception(backoff.expo, conn_err_redis, max_tries=5)
+    async def set_array(self, key, array):
+        await self.redis.lpush(key, *array)
+
+    @backoff.on_exception(backoff.expo, conn_err_redis, max_tries=5)
+    async def get_array(self, key):
+        array = await self.redis.lrange(key, 0, -1)
+        return array
+
+    @backoff.on_exception(backoff.expo, conn_err_redis, max_tries=5)
+    async def set_user_hash(self, user_id, user_data):
+        # user_data = json.dumps({'role': role_id,
+        #                         'username': user_name,
+        #                         'first_name': first_name,
+        #                         "last_name": last_name,
+        #                         "created_at": created_at})
+        await self.redis.hset('users', f"{user_id}", user_data)
+
+    @backoff.on_exception(backoff.expo, conn_err_redis, max_tries=5)
+    async def read_tokens(self, user_id):
+        info: bytes = await self.redis.hget('users', user_id)
+        return json.loads(info.decode())
 
 
 redis: Union[RedisCache, None] = None
