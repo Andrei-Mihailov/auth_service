827651e0758311b0dcd6d6a3ca05ec2e596be494
Pavel Ershov <40699895+PavelErsh@users.noreply.github.com>
2024-04-18 10:50:16 +0200

    add files

---
+++ auth_service/src/api/dependencies.py
@@ -0,0 +1,39 @@
+from pydantic import BaseModel, Field
+
+
+class UserParams(BaseModel):
+    login: str = Field(description="Логин")
+    firstname: str = Field(description="Имя", default=None)
+    lastname: str = Field(description="Фамилия", default=None)
+    password: str = Field(description="Пароль")
+
+
+class UserEditParams(BaseModel):
+    login: str = Field(description="Логин", default=None)
+    firstname: str = Field(description="Имя", default=None)
+    lastname: str = Field(description="Фамилия", default=None)
+    password: str = Field(description="Пароль", default=None)
+
+
+class AuthenticationParams(BaseModel):
+    login: str = Field(description="Логин")
+    password: str = Field(description="Пароль")
+
+
+class TokenParams(BaseModel):
+    access_token: str
+    refresh_token: str
+
+
+class RoleParams(BaseModel):
+    type: str
+    permissions: int
+
+
+class RoleEditParams(BaseModel):
+    type: str = Field(description="тип", default=None)
+    permissions: int = Field(description="разрешения", default=None)
+
+
+class PermissionsParams(BaseModel):
+    name: str
