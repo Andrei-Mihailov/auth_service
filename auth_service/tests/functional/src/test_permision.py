75678ed4a13dfb87df8d2746f1ef75eb151faceb
Pavel Ershov <40699895+PavelErsh@users.noreply.github.com>
2024-04-20 10:29:43 +0200

    add tests

--- auth_service/tests/functional/src/test_permision.py
+++ auth_service/tests/functional/src/test_permision.py
@@ -1,52 +1,50 @@
 import pytest
-from fastapi.testclient import TestClient
+from httpx import AsyncClient
 from src.main import app
-
-client = TestClient(app)
-
 
 @pytest.mark.asyncio
 async def test_create_role():
-    response = await client.post("/api/v1/roles/create", json={"name": "Admin"})
-    assert response.status_code == 200
-    assert response.json()["name"] == "Admin"
-
+    async with AsyncClient(app=app, base_url="http://test") as client:
+        response = await client.post("/api/v1/roles/create", json={"name": "Admin"})
+        assert response.status_code == 200
+        assert response.json()["name"] == "Admin"
 
 @pytest.mark.asyncio
 async def test_delete_role():
-    response = await client.delete("/api/v1/roles/1")
-    assert response.status_code == 200
-
+    async with AsyncClient(app=app, base_url="http://test") as client:
+        response = await client.delete("/api/v1/roles/1")
+        assert response.status_code == 200
 
 @pytest.mark.asyncio
 async def test_change_role():
-    response = await client.put("/api/v1/roles/change/1", json={"name": "Moderator"})
-    assert response.status_code == 200
-    assert response.json()["name"] == "Moderator"
-
+    async with AsyncClient(app=app, base_url="http://test") as client:
+        response = await client.put("/api/v1/roles/change/1", json={"name": "Moderator"})
+        assert response.status_code == 200
+        assert response.json()["name"] == "Moderator"
 
 @pytest.mark.asyncio
 async def test_list_roles():
-    response = await client.get("/api/v1/roles/list")
-    assert response.status_code == 200
-    assert isinstance(response.json(), list)
-
+    async with AsyncClient(app=app, base_url="http://test") as client:
+        response = await client.get("/api/v1/roles/list")
+        assert response.status_code == 200
+        assert isinstance(response.json(), list)
 
 @pytest.mark.asyncio
 async def test_add_permissions():
-    response = await client.post("/api/v1/roles/add_permissions/1/1")
-    assert response.status_code == 200
-    assert response.json()["name"] == "Permission"
-
+    async with AsyncClient(app=app, base_url="http://test") as client:
+        response = await client.post("/api/v1/roles/add_permissions/1/1")
+        assert response.status_code == 200
+        assert response.json()["name"] == "Permission"
 
 @pytest.mark.asyncio
 async def test_add_user_role():
-    response = await client.post("/api/v1/roles/set/1/1")
-    assert response.status_code == 200
-    assert response.json()["name"] == "UserRole"
-
+    async with AsyncClient(app=app, base_url="http://test") as client:
+        response = await client.post("/api/v1/roles/set/1/1")
+        assert response.status_code == 200
+        assert response.json()["name"] == "UserRole"
 
 @pytest.mark.asyncio
 async def test_delete_permissions():
-    response = await client.delete("/api/v1/roles/1/1")
-    assert response.status_code == 200
+    async with AsyncClient(app=app, base_url="http://test") as client:
+        response = await client.delete("/api/v1/roles/1/1")
+        assert response.status_code == 200
